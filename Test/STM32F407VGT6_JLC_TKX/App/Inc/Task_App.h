9c87e613b162d5ed15de92972c39ab01fbaca469
NintyNine99 <2570955716@qq.com>
2025-09-12 12:44:12 +0800

    将测试单独做了一个分支

--- Test/STM32F407VGT6_JLC_TKX/App/Inc/Task_App.h
+++
@@ -1,8 +0,0 @@
-#ifndef __Task_App_h
-#define __Task_App_h
-
-#include "SysConfig.h"
-
-bool TaskCreation(void);
-
-#endif
--- Test/STM32F407VGT6_JLC_TKX/download.ps1
+++
@@ -1,3 +0,0 @@
-probe-rs download --chip STM32F407VG ./build/Debug/STM32F407VGT6_JLC_TKX.elf
-
-probe-rs reset --chip STM32F407VG
