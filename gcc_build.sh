33ce9e756de58c1fca917ddc875bf0052640fb3e
N1netyNine99 <n1netynine99@qq.com>
2026-01-09 16:34:16 +0800

    修改了编译脚本，默认启用debug，让编译器生成对应的调试信息

--- gcc_build.sh
+++ gcc_build.sh
@@ -1,3 +1,9 @@
 # 路径中需要有 `gcc-arm-none-eabi`
-cmake -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE="cmake/gcc-arm-none-eabi.cmake" -DLINKER_SCRIPT="L1_MCU/STM32F429ZIT6_GCC/STM32F429XX_FLASH.ld" -DMCU_MODEL="STM32F429ZIT6_GCC"
+cmake -B build -G Ninja\
+      -DCMAKE_TOOLCHAIN_FILE="cmake/gcc-arm-none-eabi.cmake"\
+      -DCMAKE_BUILD_TYPE=Debug\
+      -DLINKER_SCRIPT="L1_MCU/STM32F429ZIT6_GCC/STM32F429XX_FLASH.ld" \
+      -DMCU_MODEL="STM32F429ZIT6_GCC"
+
+
 ninja -C build
--- starm_clang_build.sh
+++ starm_clang_build.sh
@@ -1,3 +1,8 @@
 # 路径中需要有 `starm-clang`(ST官方的STM32CubeCLT中可以下载)
-cmake -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE="cmake/starm-clang.cmake" -DLINKER_SCRIPT="L1_MCU/STM32F429ZIT6_STARM/STM32F429XX_FLASH.ld" -DMCU_MODEL="STM32F429ZIT6_STARM"
+cmake -B build -G Ninja\
+      -DCMAKE_TOOLCHAIN_FILE="cmake/starm-clang.cmake"\
+      -DCMAKE_BUILD_TYPE=Debug\
+      -DLINKER_SCRIPT="L1_MCU/STM32F429ZIT6_STARM/STM32F429XX_FLASH.ld" \
+      -DMCU_MODEL="STM32F429ZIT6_STARM"
+
 ninja -C build
