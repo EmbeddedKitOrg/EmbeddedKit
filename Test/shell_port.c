cd50818c3d9b0e8744e9f5117fd53aa9503e00fe
N1netyNine99 <n1netynine99@qq.com>
2026-01-20 14:27:34 +0800

    [fix]解决 git action的错误，升级了 windows的默认arm-none-eabi-gcc版本

--- Test/shell_port.c
+++ Test/shell_port.c
@@ -1,6 +1,11 @@
-#if defined(__linux__) || defined(__APPLE__) || defined(WIN32)
 // 解决 letter shell 的 ld 报错
+#if defined(__linux__) || defined(__APPLE__)
+
 unsigned long _shell_command_start = 0;
 extern unsigned long _shell_command_end __attribute__((alias("_shell_command_start")));
+
+#elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+int _shell_command_start;
+int _shell_command_end;
 
 #endif
