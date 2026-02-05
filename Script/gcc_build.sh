c75b1a1e542158525f83df7d243af8cff29d6d80
N1netyNine99 <n1netynine99@qq.com>
2026-02-05 22:46:27 +0800

    [update]支持使用 just 来快速执行任务

--- Script/gcc_build.sh
+++ Script/gcc_build.sh
@@ -1,13 +1,4 @@
 # 路径中需要有 `gcc-arm-none-eabi`
-# cmake -B build -G Ninja\
-#       -DCMAKE_TOOLCHAIN_FILE="cmake/gcc-arm-none-eabi.cmake"\
-#       -DCMAKE_BUILD_TYPE=Debug\
-#       -DLINKER_SCRIPT="L1_MCU/STM32F429ZIT6_GCC/stm32f429xx_flash.ld" \
-#       -DMCU_MODEL="STM32F429ZIT6_GCC"\
-#       -DUSE_FREERTOS=OFF\
-#       -DUSE_FATFS=OFF\
-#       -DUSE_LVGL=OFF\
-
 cmake -B build -G Ninja\
       -DCMAKE_TOOLCHAIN_FILE="cmake/gcc-arm-none-eabi.cmake"\
       -DCMAKE_BUILD_TYPE=Debug\
