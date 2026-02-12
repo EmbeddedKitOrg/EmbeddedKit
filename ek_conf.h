301063b0f2025b135dd946c01af101eddad9efa5
N1netyNine99 <n1netynine99@qq.com>
2026-02-13 01:02:31 +0800

    [update]修复组件测试的问题

--- ek_conf.h
+++ ek_conf.h
@@ -34,7 +34,7 @@
  * - EK_STACK_ENABLE: 使能栈模块
  * - EK_SHELL_ENABLE: 使能shell模块   
  * ======================================================================== */
-#define EK_EXPORT_ENABLE  (1)
+#define EK_EXPORT_ENABLE  (0)
 #define EK_IO_ENABLE      (1)
 #define EK_STR_ENABLE     (1)
 #define EK_LOG_ENABLE     (1)
