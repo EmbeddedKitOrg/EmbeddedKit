0eca4886b200cd5d69d2a15a1401bae8287b2c93
N1netyNine99 <2570955716@qq.com>
2026-03-09 11:43:08 +0800

    [fmt]

--- L2_Core/utils/src/ek_str.c
+++ L2_Core/utils/src/ek_str.c
@@ -20,31 +20,7 @@
             if ((idx) > (len)) (idx) = (len); \
         } while (0)
 
-static bool _ek_str_ensure_cap(ek_str_t *s, uint32_t len)
-{
-    ek_assert_param(s != NULL);
-
-    if (s->cap >= len) return true;
-
-    uint32_t new_cap = s->cap;
-    do
-    {
-        if (new_cap == 0) new_cap = 2;
-        new_cap += new_cap / 2;
-    } while (new_cap < len);
-
-    char *buf = NULL;
-
-    if (s->buf == NULL) buf = ek_malloc(len);
-    else buf = ek_realloc(s->buf, new_cap);
-
-    if (buf == NULL) return false;
-
-    s->buf = buf;
-    s->cap = new_cap;
-
-    return true;
-}
+static bool _ek_str_ensure_cap(ek_str_t *s, uint32_t len);
 
 ek_str_t *ek_str_create(const char *str)
 {
@@ -233,6 +209,32 @@ int ek_str_ncmp(ek_str_t *s1, ek_str_t *s2, size_t n)
     if (n > shorter_len) n = shorter_len;
 
     return strncmp(ek_str_get_cstring(s1), ek_str_get_cstring(s2), n);
+}
+
+static bool _ek_str_ensure_cap(ek_str_t *s, uint32_t len)
+{
+    ek_assert_param(s != NULL);
+
+    if (s->cap >= len) return true;
+
+    uint32_t new_cap = s->cap;
+    do
+    {
+        if (new_cap == 0) new_cap = 2;
+        new_cap += new_cap / 2;
+    } while (new_cap < len);
+
+    char *buf = NULL;
+
+    if (s->buf == NULL) buf = ek_malloc(len);
+    else buf = ek_realloc(s->buf, new_cap);
+
+    if (buf == NULL) return false;
+
+    s->buf = buf;
+    s->cap = new_cap;
+
+    return true;
 }
 
 #endif // EK_STR_ENABLE
